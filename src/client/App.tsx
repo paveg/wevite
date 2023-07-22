cb2edc71eeaaaf5b14d33e5c71219d0f8f8668d8
Ryota Ikezawa <pavegy@gmail.com>
2023-07-22 15:16:21 +0900

    :recycle: Update combo

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -4,7 +4,6 @@ import { useState } from "react";
 import { User, useUsersIndex } from "./models/generated/users";
 
 function App() {
-  const [count, setCount] = useState(0);
   const { isLoading, error, data } = useUsersIndex();
   let users: User[] = [];
   if (!isLoading) {
@@ -13,8 +12,8 @@ function App() {
   return (
     <div className="App">
       <h1 className="text-3xl font-bold">Test</h1>
-      <div class="container mx-auto p-4">
-        <ul class="list-none hover:list-decimal">
+      <div className="container mx-auto p-4">
+        <ul className="list-none hover:list-decimal">
           {users.map((user: User) => <li key={user.id}>{user.name}</li>)}
         </ul>
       </div>
